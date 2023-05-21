e7587d5c156e75b2da512c394b43f82dca755f0d
ehsanghaffarii <ghafari.5000@gmail.com>
2023-05-21 06:33:21 +0330

    update docker

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 require("dotenv").config(); 
 
-const url = 'mongodb://localhost:27017/test'
-// const url = process.env.DB
 mongoose.set('strictQuery', true);
 module.exports = async () => {
-
   try {
     const connectionParams = {
       useNewUrlParser: true,

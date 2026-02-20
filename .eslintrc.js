021d921c08f5bbb4aee43abf4130e09a004cb4da
ehsanghaffarii <ghafari.5000@gmail.com>
2026-02-20 23:45:21 +0330

    infra: configure TypeScript build infrastructure
    - Update tsconfig.json for TypeScript 5.x with proper compiler options
    - Add build and typecheck scripts to package.json
    - Install missing @types packages (express, cors, jsonwebtoken, etc.)
    - Configure ESLint for TypeScript with strict rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,52 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
-    es2021: true
+    es2022: true,
+    node: true,
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard'],
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
+    ecmaVersion: 2022,
+    sourceType: 'module',
   },
-  rules: {
-  },
-  overrides: [{
-    files: ['*.d.ts'],
-    extends: [
-      'standard-with-typescript'
-    ],
-    parserOptions: {
-      project: 'tsconfig.json'
-    }
-  }]
-}
+  rules: {},
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'standard-with-typescript',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/no-misused-promises': 'error',
+      },
+    },
+    {
+      files: ['*.d.ts'],
+      extends: ['standard-with-typescript'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+  ignorePatterns: ['dist', 'node_modules', '*.config.js'],
+};
